2a6617c7b17d016fb0b9fe4cf55340a15a542a18
JWHI7830 <white.2893@osu.edu>
2020-09-23 20:21:44 -0400

    SPRINT 2- Initial Interfaces commit. Added the auto generated .gitignore, as well. Change in game main included to ensure compilation.

---
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/ILinkState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrossPlatformDesktopProject
+{
+    public interface ILinkState
+    {
+        void Update();
+
+        /* This Draw method's parameters are from the other sprite drawing class*/
+        /* Change as needed*/
+        void Draw(Texture2D img, SpriteBatch spriteBatch, Vector2 vector);
+    }
+}
