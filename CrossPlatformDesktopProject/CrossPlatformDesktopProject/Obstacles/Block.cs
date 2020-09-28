258b8e24a524d67ae5afa9c6165a6b881c4aa63d
Brent Hasseman <60267915+bhasseman@users.noreply.github.com>
2020-09-28 15:01:30 -0400

    Implemented Obstacle classes
    Implemented the Block and Statue obstacle classes from IObstacle interface. Also created ObstacleTextureStorage class for spritefactory, similar to LinkTextureStorage.

---
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Obstacles/Block.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrossPlatformDesktopProject.Link;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrossPlatformDesktopProject.Obstacles
+{
+    class Block : IObstacle
+    {
+        public float xPos, yPos;
+        public Block()
+        {
+            xPos = 200;
+            yPos = 200;
+        }
+        public void Update()
+        {
+            // Not needed for blocks
+        }
+
+        public void Draw(SpriteBatch spriteBatch, float xPos, float yPos)
+        {
+            Texture2D texture = ObstacleTextureStorage.Instance.getObstacleSpriteSheet();
+            Rectangle source = ObstacleTextureStorage.OBSTACLE_BLOCK;
+            Rectangle destination = new Rectangle((int)xPos, (int)yPos, source.Width * 3, source.Height * 3);
+            spriteBatch.Draw(texture, destination, source, Color.White);
+        }
+    }
+}
