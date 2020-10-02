77be44ed4d2f9dfe1667b04336ee22abd2fb9d5f
James Cross <cross.505@buckeyemail.osu.edu>
2020-10-02 18:45:37 -0400

    Update commans with Reduced ILink States
    ILink has been updated to have methods for each actionL
    MoveLeft
    MoveRight
    MoveUp
    MoveDown
    UsePrimary
    UseSecondary
    
    Each method is called directly from the keyboard controller

--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/Link/LinkFacingEastState.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Link/LinkFacingEastState.cs
@@ -32,34 +32,44 @@ namespace CrossPlatformDesktopProject.Link
             spriteBatch.Draw(texture, destination, source, Color.White);
         }
 
-        void ILinkState.Update(ISet<ButtonKind> buttons)
+        void ILinkState.Update()
         {
-            if (buttons.Contains(ButtonKind.PRIMARY))
+        }
+
+        public void MoveDown()
+        {
+            player.currentState = new LinkFacingSouthState(player);
+        }
+
+        public void MoveLeft()
+        {
+            player.currentState = new LinkFacingWestState(player);
+        }
+
+        public void MoveRight()
+        {
+            player.xPos += Player.walking_speed;
+            if (--frames_left <= 0)
             {
-                player.currentState = new LinkSword1East(player);
-            }
-            else if (buttons.Contains(ButtonKind.RIGHT))
-            {
-                player.xPos += Player.walking_speed;
-                if (--frames_left <= 0)
-                {
-                    frames_left = Player.frames_per_step;
-                    my_source_index++;
-                    my_source_index %= my_sources.Count;
-                }
-            }
-            else if (buttons.Contains(ButtonKind.DOWN))
-            {
-                player.currentState = new LinkFacingSouthState(player);
-            }
-            else if (buttons.Contains(ButtonKind.UP))
-            {
-                player.currentState = new LinkFacingNorthState(player);
-            }
-            else if (buttons.Contains(ButtonKind.LEFT))
-            {
-                player.currentState = new LinkFacingWestState(player);
+                frames_left = Player.frames_per_step;
+                my_source_index++;
+                my_source_index %= my_sources.Count;
             }
         }
+
+        public void MoveUp()
+        {
+            player.currentState = new LinkFacingNorthState(player);
+        }
+
+        public void UsePrimary()
+        {
+            player.currentState = new LinkSword1East(player);
+        }
+
+        public void UseSecondary()
+        {
+        }
+
     }
 }
--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/Link/LinkFacingSouthState.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Link/LinkFacingSouthState.cs
@@ -4,21 +4,28 @@ using System.Collections.Generic;
 
 namespace CrossPlatformDesktopProject.Link
 {
-    class LinkFacingSouthState1 : ILinkState
+    class LinkFacingSouthState : ILinkState
     {
         private Player player;
         private int frames_left;
+        private static List<Rectangle> my_sources = new List<Rectangle>
+        {
+            LinkTextureStorage.LINK_IDLE_SOUTH,
+            LinkTextureStorage.LINK_STEP_SOUTH,
+        };
+        private int my_source_index;
 
-        public LinkFacingSouthState1(Player player)
+        public LinkFacingSouthState(Player player)
         {
             this.player = player;
             frames_left = Player.frames_per_step;
+            my_source_index = 0;
         }
 
         void ILinkState.Draw(SpriteBatch spriteBatch, float xPos, float yPos)
         {
             Texture2D texture = LinkTextureStorage.Instance.getLinkSpriteSheet();
-            Rectangle source = LinkTextureStorage.LINK_IDLE_SOUTH;
+            Rectangle source = my_sources[my_source_index];
             Rectangle destination = new Rectangle(
                 (int)xPos, (int)yPos,
                 source.Width * 3, source.Height * 3);
@@ -33,23 +40,25 @@ namespace CrossPlatformDesktopProject.Link
             player.yPos += Player.walking_speed;
             if (--frames_left <= 0)
             {
-                player.currentState = new LinkFacingSouthState2(player);
+                frames_left = Player.frames_per_step;
+                my_source_index++;
+                my_source_index %= my_sources.Count;
             }
         }
 
         public void MoveLeft()
         {
-            player.currentState = new LinkFacingWestState1(player);
+            player.currentState = new LinkFacingWestState(player);
         }
 
         public void MoveRight()
         {
-            player.currentState = new LinkFacingEastState1(player);
+            player.currentState = new LinkFacingEastState(player);
         }
 
         public void MoveUp()
         {
-            player.currentState = new LinkFacingNorthState1(player);
+            player.currentState = new LinkFacingNorthState(player);
         }
 
         public void UsePrimary()
