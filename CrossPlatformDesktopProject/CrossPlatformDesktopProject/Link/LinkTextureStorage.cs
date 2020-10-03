572b10f3b247a8556c9454e6b1dcd5a664646c63
Brent Hasseman <60267915+bhasseman@users.noreply.github.com>
2020-10-02 20:39:46 -0400

    Added storage of bomb textures

--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/Link/LinkTextureStorage.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Link/LinkTextureStorage.cs
@@ -73,5 +73,10 @@ namespace CrossPlatformDesktopProject.Link
         public static Rectangle BOOMERANG_2 = new Rectangle(73, 189, 7, 7);
         public static Rectangle BOOMERANG_3 = new Rectangle(82, 189, 7, 7);
         //ADD MIRRORED BOOMERANG FOR FULL ROTATION
+
+        public static Rectangle BOMB = new Rectangle(129, 185, 8, 14);
+        public static Rectangle BOMB_EXPLOSION_1 = new Rectangle(138, 185, 16, 16);
+        public static Rectangle BOMB_EXPLOSION_2 = new Rectangle(155, 185, 16, 16);
+        public static Rectangle BOMB_EXPLOSION_3 = new Rectangle(172, 185, 16, 16);
     }
 }
