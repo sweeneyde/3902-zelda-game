a084904b134b86f4ced76df52e55c9bc560c0edf
Brent Hasseman <60267915+bhasseman@users.noreply.github.com>
2020-10-03 23:45:35 -0400

    Updated Commands to have 3 use items

---
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Commands/TakeDamageCommand.cs
@@ -0,0 +1,19 @@
+﻿using CrossPlatformDesktopProject.Link;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrossPlatformDesktopProject.Commands
+{
+    class TakeDamageCommand : ICommand
+    {
+        private Player myPlayer;
+        public TakeDamageCommand(Player player)
+        {
+            myPlayer = player;
+        }
+        public void Execute() => myPlayer.TakeDamage();
+    }
+}
