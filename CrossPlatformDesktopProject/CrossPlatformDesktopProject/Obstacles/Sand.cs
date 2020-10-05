f4b9bf33a8c39f6009992da6d68fd91a07c32c23
JWHI7830 <white.2893@osu.edu>
2020-10-05 10:55:50 -0400

    Acceptable amount of Obstacles added.

---
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Obstacles/Sand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrossPlatformDesktopProject.Link;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrossPlatformDesktopProject.Obstacles
+{
+    class Sand : IObstacle
+    {
+        public float xPos, yPos;
+        public Sand()
+        {
+            xPos = 200;
+            yPos = 200;
+        }
+        public void Update()
+        {
+            // Not needed for blocks
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Texture2D texture = ObstacleTextureStorage.Instance.getObstacleSpriteSheet();
+            Rectangle source = ObstacleTextureStorage.OBSTACLE_SAND;
+            Rectangle destination = new Rectangle((int)xPos, (int)yPos, source.Width * 3, source.Height * 3);
+            spriteBatch.Draw(texture, destination, source, Color.White);
+        }
+    }
+}
