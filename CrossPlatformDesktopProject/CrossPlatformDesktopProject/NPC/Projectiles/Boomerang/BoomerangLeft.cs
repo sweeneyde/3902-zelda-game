3bac39f8820dc17687f1f3b0c3a0911828c9d6b2
royjpark83 <royjpark83@gmail.com>
2020-10-26 18:28:07 -0400

    pull

---
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/NPC/Projectiles/Boomerang/BoomerangLeft.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace CrossPlatformDesktopProject.NPC
+{
+    class BoomerangLeft : INpcState
+    {
+        private int my_frame_index;
+        private int delay_frame_index;
+        private int starting;
+        private GoriyaBoomerang boomerang;
+
+        private static int delay_frames = 4;
+
+        private static List<Rectangle> my_source_frames = new List<Rectangle>{
+            NpcTextureStorage.BOOMERANG_1,
+            NpcTextureStorage.BOOMERANG_2,
+            NpcTextureStorage.BOOMERANG_3,
+        };
+
+        public BoomerangLeft(GoriyaBoomerang boomerang, float xPos, float yPos, bool start)
+        {
+            this.boomerang = boomerang;
+            my_frame_index = 0;
+            delay_frame_index = 0;
+
+            starting = (int)xPos;
+
+            boomerang.xPos = xPos;
+            boomerang.yPos = yPos;
+            boomerang.start = start;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, float xPos, float yPos)
+        {
+            if (boomerang.start == true)
+            {
+                Texture2D texture = NpcTextureStorage.Instance.getEnemySpriteSheet();
+
+                Rectangle source = my_source_frames[my_frame_index];
+                Rectangle destination_fireball = new Rectangle(
+                    (int)boomerang.xPos, (int)boomerang.yPos,
+                    NpcTextureStorage.GORIYA_LEFT_1.Width * 2, NpcTextureStorage.GORIYA_LEFT_1.Height * 3);
+
+                spriteBatch.Draw(texture, destination_fireball, source, Color.White);
+            }
+        }
+
+        public void Update()
+        {
+            if (boomerang.start == true)
+            {
+                if (++delay_frame_index >= delay_frames)
+                {
+                    delay_frame_index = 0;
+                    my_frame_index++;
+
+                    if (boomerang.travelmarker == 1 && boomerang.xPos == starting)
+                    {
+                        boomerang.start = false;
+                    }
+
+                    if (boomerang.travelmarker == 0)
+                    {
+                        boomerang.xPos -= 10;
+                    }
+                    else if (boomerang.travelmarker == 1)
+                    {
+                        boomerang.xPos += 10;
+                    }
+
+                    if (boomerang.xPos == 0)
+                    {
+                        boomerang.travelmarker = 1;
+                    }
+
+                    my_frame_index %= my_source_frames.Count;
+                }
+            }
+        }
+    }
+}
