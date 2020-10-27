e96aac0315f7b8bcfff35c7776f391bff899ad97
royjpark83 <royjpark83@gmail.com>
2020-10-26 23:11:25 -0400

    removing magic numbers

--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/NPC/Bat/BatWalkWest.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/NPC/Bat/BatWalkWest.cs
@@ -8,6 +8,7 @@ namespace CrossPlatformDesktopProject.NPC
     {
         private int my_frame_index;
         private int delay_frame_index;
+        private int counter;
         private Bat bat;
 
         private static int delay_frames = 10;
@@ -35,7 +36,7 @@ namespace CrossPlatformDesktopProject.NPC
 
         public void Update()
         {
-            if (bat.xPos == 400 && bat.yPos == 150)
+            if (counter == 10)
             {
                 bat.currentState = new BatWalkNorth(bat);
             }
@@ -44,6 +45,7 @@ namespace CrossPlatformDesktopProject.NPC
             {
                 delay_frame_index = 0;
                 bat.xPos -= 5;
+                counter++;
                 my_frame_index++;
                 my_frame_index %= my_source_frames.Count;
             }
--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/NPC/Skeleton/SkeletonWalkNorth.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/NPC/Skeleton/SkeletonWalkNorth.cs
@@ -8,6 +8,7 @@ namespace CrossPlatformDesktopProject.NPC
     {
         private int my_frame_index;
         private int delay_frame_index;
+        private int counter;
         private Skeleton skeleton;
 
         private static int delay_frames = 10;
@@ -35,7 +36,7 @@ namespace CrossPlatformDesktopProject.NPC
 
         public void Update()
         {
-            if (skeleton.xPos == 400 && skeleton.yPos == 100)
+            if (counter == 10)
             {
                 skeleton.currentState = new SkeletonWalkEast(skeleton);
             }
@@ -44,6 +45,7 @@ namespace CrossPlatformDesktopProject.NPC
             {
                 delay_frame_index = 0;
                 skeleton.yPos -= 5;
+                counter++;
                 my_frame_index++;
                 my_frame_index %= my_source_frames.Count;
             }
