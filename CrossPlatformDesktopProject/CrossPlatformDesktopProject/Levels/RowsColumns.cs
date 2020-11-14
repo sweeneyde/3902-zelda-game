8e2c3f53b0689ceffc23d1dd94f70add256f335c
JWHI7830 <white.2893@osu.edu>
2020-11-14 14:08:19 -0500

    rows columns working as expected

--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/Levels/RowsColumns.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Levels/RowsColumns.cs
@@ -8,11 +8,13 @@ namespace CrossPlatformDesktopProject.Levels
 {
     class RowsColumns
     {
-        private const int X_ORIGIN = (RoomTextureStorage.BORDER_WIDTH - RoomTextureStorage.ROOM_WIDTH) / 2;
-        private const int Y_ORIGIN = (RoomTextureStorage.BORDER_HEIGHT - RoomTextureStorage.ROOM_WIDTH) / 2;
+        private const int X_ORIGIN = (RoomTextureStorage.BORDER_WIDTH - RoomTextureStorage.ROOM_WIDTH) / 4;
+        private const int Y_ORIGIN = (RoomTextureStorage.BORDER_HEIGHT - RoomTextureStorage.ROOM_HEIGHT) / 4;
         public static float[] ConvertRowsColumns(int row, int column)
         {
-            float[] coords = { X_ORIGIN * row, Y_ORIGIN * column };
+            row++;
+            column++;
+            float[] coords = { X_ORIGIN * column, Y_ORIGIN * row };
 
             return coords;
             
