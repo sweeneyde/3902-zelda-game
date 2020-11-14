fa5d2e3812e588d10ebf93689ef4a4971f121dd4
JWHI7830 <white.2893@osu.edu>
2020-11-14 16:01:28 -0500

    All obstacles placed in game positions.

---
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Obstacles/Transparent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrossPlatformDesktopProject.Link;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CrossPlatformDesktopProject.Obstacles
+{
+    class Transparent : IObstacle
+    {
+        public float xPos, yPos;
+        public Transparent(float xPos, float yPos)
+        {
+            this.xPos = xPos;
+            this.yPos = yPos;
+        }
+        public void Update()
+        {
+            // Not needed for blocks
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Texture2D texture = ObstacleTextureStorage.Instance.getObstacleSpriteSheet();
+            Rectangle source = ObstacleTextureStorage.OBSTACLE_TRANSPARENT;
+            Rectangle destination = new Rectangle((int)xPos, (int)yPos, source.Width, source.Height);
+            spriteBatch.Draw(texture, destination, source, Color.Transparent);
+        }
+
+        public Rectangle GetRectangle()
+        {
+            Rectangle source = ObstacleTextureStorage.OBSTACLE_TRANSPARENT;
+            return new Rectangle((int)xPos, (int)yPos, source.Width, source.Height);
+        }
+    }
+}
