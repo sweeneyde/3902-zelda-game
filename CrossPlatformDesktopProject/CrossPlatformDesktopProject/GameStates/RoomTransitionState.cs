d7df041957b37a63c463397ae42cf53e3450326c
Brent Hasseman <hasseman.5@osu.edu>
2020-11-15 18:37:32 -0500

    Scrolling to Left Transition Added
    Rooms and borders scroll when door is touched, but currently always scroll to left. Also, doors are not drawn with scrolling yet so they are colored rectangle placeholders.

--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/GameStates/RoomTransitionState.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/GameStates/RoomTransitionState.cs
@@ -11,20 +11,40 @@ namespace CrossPlatformDesktopProject
         Room room1, room2;
         int delayLeft;
         RenderTarget2D screenshot;
+        int scrolling;
+        Texture2D texture;
+        Rectangle sourceRoom1;
+        Rectangle sourceRoom2;
+        Rectangle destinationRoom1;
+        Rectangle destinationRoom2;
+        int scrollSpeed;
+
         public RoomTransitionState(Game1 game, Room room1, Room room2)
         {
             this.game = game;
             this.room1 = room1;
             this.room2 = room2;
-            delayLeft = 20;
+            scrolling = 0;
+            delayLeft = 112;
             screenshot = GameScreenTextureStorage.Instance.screenshot;
+            texture = RoomTextureStorage.Instance.getRoomSpriteSheet();
+            sourceRoom1 = RoomTextureStorage.ROOM_RECTS[room1.roomID];
+            sourceRoom2 = RoomTextureStorage.ROOM_RECTS[room2.roomID];
+            scrollSpeed = 2;
         }
 
         public void Draw(SpriteBatch sb)
         {
-
-            Rectangle window = new Rectangle(0, 0, game.GraphicsDevice.Viewport.Width, game.GraphicsDevice.Viewport.Height);
-            sb.Draw(screenshot, window, window, Color.White);
+            Rectangle borderSource = RoomTextureStorage.BORDER;
+            Rectangle borderDestination1 = new Rectangle(scrolling, 0, RoomTextureStorage.BORDER_WIDTH, RoomTextureStorage.BORDER_HEIGHT);
+            Rectangle borderDestination2 = new Rectangle(scrolling + (RoomTextureStorage.ROOM_WIDTH + RoomTextureStorage.BORDER_WIDTH) / 2, 0, RoomTextureStorage.BORDER_WIDTH, RoomTextureStorage.BORDER_HEIGHT);
+            destinationRoom1 = new Rectangle((RoomTextureStorage.BORDER_WIDTH - RoomTextureStorage.ROOM_WIDTH) / 2 + scrolling, (RoomTextureStorage.BORDER_HEIGHT - RoomTextureStorage.ROOM_HEIGHT) / 2, RoomTextureStorage.ROOM_WIDTH, RoomTextureStorage.ROOM_HEIGHT);
+            destinationRoom2 = new Rectangle((RoomTextureStorage.BORDER_WIDTH - RoomTextureStorage.ROOM_WIDTH) / 2 + scrolling + (RoomTextureStorage.ROOM_WIDTH + RoomTextureStorage.BORDER_WIDTH) / 2, (RoomTextureStorage.BORDER_HEIGHT - RoomTextureStorage.ROOM_HEIGHT) / 2, RoomTextureStorage.ROOM_WIDTH, RoomTextureStorage.ROOM_HEIGHT);
+            sb.Draw(texture, borderDestination1, borderSource, Color.White);
+            sb.Draw(texture, borderDestination2, borderSource, Color.White);
+            sb.Draw(texture, destinationRoom1, sourceRoom1, Color.White);
+            sb.Draw(texture, destinationRoom2, sourceRoom2, Color.White);
+            
         }
 
         public void Update()
@@ -33,6 +53,8 @@ namespace CrossPlatformDesktopProject
             {
                 game.currentState = game.currentGamePlayState;
             }
+            scrolling -= scrollSpeed;
+            
         }
     }
 }
