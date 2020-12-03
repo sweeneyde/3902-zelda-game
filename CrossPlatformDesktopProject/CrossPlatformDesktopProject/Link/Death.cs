6550e5668ef48d40ea0446c7cbca01b25f9110a3
Brent Hasseman <hasseman.5@osu.edu>
2020-12-03 18:08:31 -0500

    Initial Death Class
    Empty class that will draw dying Link animation, will have similar logic to Bomb explosion

---
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Link/Death.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using CrossPlatformDesktopProject.Commands;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace CrossPlatformDesktopProject.Link
+{
+	public class Death
+	{
+        private Player player;
+        private Vector2 currentPos;
+        private Game1 myGame;
+        private float usedX;
+        private float usedY;
+        private int my_frame_index;
+        private int delay_frame_index;
+        private int delay_frames;
+        private int explode_time;
+        private int current_frame;
+        private int swap_frame;
+        private int swap_frame_index;
+
+        private static List<Rectangle> my_source_frames = new List<Rectangle>
+        {
+            LinkTextureStorage.LINK_IDLE_SOUTH,
+            LinkTextureStorage.LINK_IDLE_EAST,
+            LinkTextureStorage.LINK_IDLE_NORTH,
+            LinkTextureStorage.MIRRORED_LINK_IDLE_WEST
+        };
+
+        public Death(Player player, Game1 game)
+		{
+
+		}
+	}
+}
