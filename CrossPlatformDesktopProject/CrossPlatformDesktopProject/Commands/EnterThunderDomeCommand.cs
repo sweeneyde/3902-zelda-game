1ef2f7b1aff160e91713777e57ce1d172c24b6ba
wchiraz <61662753+wchiraz@users.noreply.github.com>
2020-12-04 23:24:56 -0500

    Music to ThunderDome

--- CrossPlatformDesktopProject/CrossPlatformDesktopProject/Commands/EnterThunderDomeCommand.cs
+++ CrossPlatformDesktopProject/CrossPlatformDesktopProject/Commands/EnterThunderDomeCommand.cs
@@ -1,6 +1,7 @@
 ﻿using CrossPlatformDesktopProject.CollisionHandler;
 using CrossPlatformDesktopProject.Levels;
 using CrossPlatformDesktopProject.Link;
+using CrossPlatformDesktopProject.Sound;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 namespace CrossPlatformDesktopProject.Commands
 {
     class EnterThunderDome : ICommand
-    {
+    { 
         private Game1 myGame;
         public EnterThunderDome(Game1 game)
         {
@@ -18,6 +19,10 @@ namespace CrossPlatformDesktopProject.Commands
         }
         public void Execute()
         {
+            SoundStorage.music_instance.Stop();
+            SoundStorage.music_instance = SoundStorage.Instance.getThunderSound().CreateInstance();
+            SoundStorage.music_instance.IsLooped = true;
+            SoundStorage.music_instance.Play();
             myGame.currentGamePlayState = new GamePlayState(myGame, Room.FromId(myGame, "018"));
             myGame.currentState = new ThunderDomeState(myGame, Room.FromId(myGame, "018"));
         }
